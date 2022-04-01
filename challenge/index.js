890c88c5d0c44497cc5f5ee3a47082ea62915f57
David Stavis <davidstavis@gmail.com>
2022-03-31 20:27:52 -0600

    Complete mod 0 paired challenge

---
+++ challenge/index.js
@@ -0,0 +1,15 @@
+// We debated a few ideas, but ended up going with index.js because it would interact with index.md
+
+// Declare the variable checkedOut and set it to the boolean false. This will help you know if a book has been checked out
+var checkedOut = false
+var genre = "Horror"
+
+// This variable tells you how many books the library contains in this genre
+var horrorBooks = 1275
+// Declare the variable paperback and assign to it the value of the boolean false
+var paperback = true
+var authorName = "David Foster Wallace"
+var pageNumber = 6
+
+// Declare the variable "sections", and assign it the value of an array. This array contains 4 items, all of which are strings
+var sections = ["Young Adult Fiction", "Science", "Sci-fi", "Fantasy"]
---
+++ challenge/index.md
@@ -0,0 +1,14 @@
+# Book Titles
+- Infinite Jest
+- Watership Down
+- Cloud Atlas
+- Eat, Poop, Sleep
+- Eat, Pray, Love
+
+
+I (David) drove
+Eddie navigated
+
+We didn't really run into any questions
+Figuring out which lines we wanted to annotate was annoying
+David forked the repo and ran git clone on his other screen, so Eddie wasn't able to see what he did and it was confusing
